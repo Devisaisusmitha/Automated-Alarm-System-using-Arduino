6fffdbb71ebdf6ebc6f306335d13514b3b5e09c2
Devisaisusmitha <devikanisetty661@gmail.com>
2026-04-29 23:43:21 +0530

    Add files via upload
     Interrupt-Based Automated Alarm System project repository.

---
+++ automated_alarm_system1.ino
@@ -0,0 +1,40 @@
+volatile boolean ledOn = false;
+int o_BUZZERPin = 8;
+int o_REDLEDPin = 13;
+int o_GREENLEDPin = 4;
+int i_PUSHButton = 2;
+
+void setup()
+{
+  pinMode(o_BUZZERPin, OUTPUT);
+   pinMode(o_REDLEDPin, OUTPUT);
+   pinMode(o_GREENLEDPin, OUTPUT);
+   pinMode(i_PUSHButton, INPUT);
+   // Turn on green led by default
+  digitalWrite(o_GREENLEDPin, HIGH);
+  //code portael maps the pin to interrupt vvector table
+  attachInterrupt(digitalPinToInterrupt(2), buttonPressed, RISING);
+}
+
+void loop()
+{
+ 
+}
+//ISR
+void buttonPressed(){
+//Turns the buzzer and red led off, green led on
+  if(ledOn)
+  {
+    digitalWrite(o_GREENLEDPin, HIGH);
+    noTone(o_BUZZERPin);
+    ledOn = false;
+    digitalWrite(o_REDLEDPin, LOW);
+           }else {
+             //Turns on red led and buzzer , green led off
+             digitalWrite(o_GREENLEDPin, LOW);
+             //261HZ
+             tone(o_BUZZERPin, 261);
+             ledOn = true;
+             digitalWrite(o_REDLEDPin, HIGH);
+           }
+           }
